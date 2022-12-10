ea611a60c74f9a8fad30561b3686151b6a9d626d
Huzaifa Raghav <huzaifaraghav2000@gmail.com>
2022-12-10 17:39:06 +0800

    Move nav names to constant

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,30 @@
-import { Center, Box, useTheme } from "@chakra-ui/react";
+import { Center, Box, useTheme, Stack, HStack, Link, StackDivider, Button } from "@chakra-ui/react";
 import Image from "next/image";
 import logo from '../public/logo.png';
+
+type NavItem = {
+    name: String;
+    href: string
+};
+
+const NAV_ITEMS: Array<NavItem> = [ 
+    {
+        name: "Interest Groups",
+        href: "/groups"
+    }, 
+    {
+        name: "NOW!",
+        href: "/now"
+    },
+    {
+        name: "Houses",
+        href: "/houses"
+    },
+    {
+        name: "Events",
+        href: "/events"
+    }
+];
 
 export default function Header() {
     const theme = useTheme();
@@ -15,9 +39,21 @@ export default function Header() {
             </Center>
 
             {/* Navbar */}
-            <Box backgroundColor={theme.colors.primary.main} color={'white'}>
+            <HStack
+                align="center"
+                justify="space-around"
+                bg={theme.colors.primary.main}
+                color="white"
+                padding={4}
+                >
+                {
+                    NAV_ITEMS.map((item: NavItem, idx) => {
+                        return <Link href={item.href} key={idx}>{item.name}</Link>
+                    })
+                }
                 
-            </Box>
+    
+            </HStack>
         </header>
     );
 };
