021e399d249820fa68e919bbb8016a9cbdf350cb
sheng <chengzhisheng2001@gmail.com>
2023-03-18 19:41:37 +0800

    refactor: Rewrite Bookings page.
    Rewrote the bookings page in grid.tsx.
    Note: does not include backend integration, month view, and booking
    edit.

--- components/BookingConfirmationPopup.tsx
+++ components/BookingConfirmationPopup.tsx
@@ -62,7 +62,7 @@ export const BookingConfirmationPopup: React.FC<BookingConfirmationPopupProps> =
 
 
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal isOpen={isOpen} onClose={onClose} autoFocus={false} returnFocusOnClose={false}>
       <ModalOverlay />
       <ModalContent>
         <ModalHeader color="white" bg="#1f407b" fontSize="1.1rem" alignItems="center">NEW BOOKING</ModalHeader>
