e5d0a5ea54dc0f470356ee63f84e631c27c9c2be
parth-io <67057645+parth-io@users.noreply.github.com>
2023-04-03 11:02:17 +0800

    Undo removal of mock Telegram signin

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -4,11 +4,39 @@ import { Button } from '@chakra-ui/react'
 import TelegramLoginButton from './TelegramLoginButton'
 import { useUserInfo } from '../utils'
 
+// { Parth: 4, Zhi Sheng: 5, Megan: 6, Conrad: 7 } based on 2 April seed file
+const NEXT_PUBLIC_BACKEND_TELEGRAM_USER_ID = 5
+const NEXT_PUBLIC_BACKEND_JWT_DEV =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTExMzY0ODY1MCwiZmlyc3RfbmFtZSI6IlBhcnRoIiwidXNlcm5hbWUiOiJncGFydGgyNiIsImF1dGhfZGF0ZSI6MTY4MDQ5MDc0NywicGhvdG9fdXJsIjoiaHR0cHM6Ly90Lm1lL2kvdXNlcnBpYy8zMjAvZ0VtVHBfbDR1WUJueWQ3elZFZTQxVGRuQWhQczgtMmJnbXY3MXc4ZzM2US5qcGciLCJoYXNoIjoiNzVkNTc3ZmNhMjk0NGM2ZmE3ZTU3OTgwOGRhZDM1Yjg4M2U3ZTRkNTkzMTlhNjM3ZGUxNjA1N2VlNzhjYzAyNSIsImlhdCI6MTY4MDQ5MDc0OSwiZXhwIjoxNjgwNDk0MzQ5fQ.spKGHWq8Mmg-jARg2J3mgsN6wupsHl6xxMfom3DeqSs'
+const NEXT_PUBLIC_NGINX_PROXY_ON = false
+
 const Auth: React.FC = () => {
   const [auth, setAuth, cleanUpAuth] = useUserInfo()
 
+  const loginButtonDev = (
+    <Button
+      onClick={() => {
+        setAuth({
+          token: NEXT_PUBLIC_BACKEND_JWT_DEV,
+          orgIds: [0],
+          userId: NEXT_PUBLIC_BACKEND_TELEGRAM_USER_ID,
+          // userInfo is not needed for now, so can just add filler values
+          userInfo: {
+            firstName: "Parth",
+            telegramId: -1,
+            photoUrl: "",
+            username: "gparth26"
+          }
+        })
+      }}
+    >
+      Login for dev
+    </Button>
+  )
+
   const loginButtonWidget = (
     <TelegramLoginButton
+      // BOT_TOKEN on the backend needs to match
       botName={process.env.NODE_ENV === 'development' ? 'TestForUSDevsBot' : 'TestForUSDevsProdBot'}
       dataOnauth={(user: TelegramUser) => {
         const headers = {
@@ -29,7 +57,7 @@ const Auth: React.FC = () => {
               firstName: userCredentials.first_name,
               telegramId: userCredentials.id,
               photoUrl: userCredentials.photo_url,
-              username: userCredentials.username
+              username: userCredentials.username,
             }
             setAuth({ token, orgIds, userInfo, userId })
           })
@@ -51,7 +79,11 @@ const Auth: React.FC = () => {
   )
 
   return auth?.token === ''
-    ? loginButtonWidget
+    ? process.env.NODE_ENV === 'development'
+      ? NEXT_PUBLIC_NGINX_PROXY_ON
+        ? loginButtonWidget
+        : loginButtonDev
+      : loginButtonWidget
     : logoutButton
 }
 export default Auth
