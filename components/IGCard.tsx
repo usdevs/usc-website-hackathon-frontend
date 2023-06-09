44d66cc19216f1ef2f07b0b1ad36f561710c32aa
parth-io <67057645+parth-io@users.noreply.github.com>
2023-06-09 22:10:55 +0700

    Fix merge conflict

--- components/IGCard.tsx
+++ components/IGCard.tsx
@@ -1,5 +1,9 @@
-import { Button, Card, CardBody, Heading, Image, Stack, Text, VStack } from '@chakra-ui/react'
 import {
+  Button,
+  Card,
+  CardBody,
+  Heading,
+  Image,
   Box,
   Center,
   Divider,
@@ -11,10 +15,11 @@ import {
   ModalCloseButton,
   ModalBody,
   ModalFooter,
+  Text,
+  VStack,
 } from '@chakra-ui/react'
 import { motion } from 'framer-motion'
 import React from 'react'
-import { ExpandableText } from './ExpandableText'
 import { FaUserCircle } from 'react-icons/fa'
 import Link from 'next/link'
 
@@ -24,11 +29,56 @@ interface IGInfoProps {
 
 const MotionBox = motion(Box)
 
+const getContactButton = (contact: string) => {
+  return (
+    <Button
+      overflow='hidden'
+      textOverflow={'ellipsis'}
+      leftIcon={<FaUserCircle />}
+      variant='outline'
+      colorScheme='blue'
+      minWidth={'7vw'}
+      maxWidth={'7vw'}
+      rounded='15px'
+      style={{
+        borderRadius: '0.5rem',
+        color: '#1f407b',
+        border: '1px solid #1f407b',
+        display: 'flex',
+        alignItems: 'center',
+      }}
+    >
+      <Text noOfLines={[1]} display='inline'>
+        {contact}
+      </Text>
+    </Button>
+  )
+}
+
+const getInviteLinkButton = (invite_link: string) => {
+  return (
+    <Link href={invite_link} rel='noopener noreferrer' target='_blank'>
+      <Button
+        overflow='hidden'
+        textOverflow={'ellipsis'}
+        variant='outline'
+        colorScheme='blue'
+        minWidth={'7vw'}
+        maxWidth={'7vw'}
+        rounded='15px'
+        style={{ borderRadius: '0.5rem', color: '#229ed9', border: '1px solid #229ed9' }}
+      >
+        Invite Link
+      </Button>
+    </Link>
+  )
+}
+
 const LeftPane: React.FC<IGInfoProps> = ({ ig_info }) => {
   const { contact, invite_link, image } = ig_info
 
   return (
-    <Box pt={'2vh'} pb={'2vh'} w='60vw'>
+    <VStack padding='1rem' borderRight='2px solid darkgrey'>
       <Center>
         <Image
           objectFit='cover'
@@ -37,42 +87,13 @@ const LeftPane: React.FC<IGInfoProps> = ({ ig_info }) => {
           alt='IG Picture'
         />
       </Center>
-      <Box pt={'1vh'}>
-        <VStack>
-          <Button
-            overflow='hidden'
-            textOverflow={'ellipsis'}
-            leftIcon={<FaUserCircle />}
-            variant='outline'
-            colorScheme='blue'
-            minWidth={'7vw'}
-            maxWidth={'7vw'}
-            rounded='15px'
-          >
-            <Text noOfLines={[1]} display='inline'>
-              {contact}
-            </Text>
-          </Button>
-          <Link href={invite_link} rel='noopener noreferrer' target='_blank'>
-            <Button
-              overflow='hidden'
-              textOverflow={'ellipsis'}
-              variant='outline'
-              colorScheme='blue'
-              minWidth={'7vw'}
-              maxWidth={'7vw'}
-              rounded='15px'
-            >
-              Invite Link
-            </Button>
-          </Link>
-        </VStack>
-      </Box>
-    </Box>
+      {getContactButton(contact)}
+      {getInviteLinkButton(invite_link)}
+    </VStack>
   )
 }
 
-const IGCard: React.FC<IGInfoProps> = (props) => {
+const IGCard: React.FC<IGInfoProps> = ({ ig_info }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
   return (
@@ -85,7 +106,7 @@ const IGCard: React.FC<IGInfoProps> = (props) => {
           shadow='md'
           onClick={onOpen}
         >
-          <LeftPane ig_info={props.ig_info} />
+          <LeftPane ig_info={ig_info} />
           <Divider
             orientation='vertical'
             borderColor='blackAlpha.400'
@@ -95,14 +116,9 @@ const IGCard: React.FC<IGInfoProps> = (props) => {
           />
           <Center>
             <CardBody>
-              <Heading
-                // color={useColorModeValue('gray.700', 'white')}
-                fontSize={'2xl'}
-                fontFamily={'body'}
-              >
-                {props.ig_info.title}
+              <Heading fontSize={'2xl'} fontFamily={'body'}>
+                {ig_info.title}
               </Heading>
-
               <Text
                 color='gray.500'
                 pt='1vh'
@@ -113,7 +129,7 @@ const IGCard: React.FC<IGInfoProps> = (props) => {
                   overflow: 'hidden',
                 }}
               >
-                {props.ig_info.description}
+                {ig_info.description}
               </Text>
             </CardBody>
           </Center>
@@ -123,44 +139,15 @@ const IGCard: React.FC<IGInfoProps> = (props) => {
       <Modal isOpen={isOpen} onClose={onClose} size='xl'>
         <ModalOverlay />
         <ModalContent>
-          <Image src={props.ig_info.image} alt='Modal Image' maxH='350px' objectFit='cover' />
-          <ModalHeader>{props.ig_info.title}</ModalHeader>
+          <Image src={ig_info.image} alt='Modal Image' maxH='350px' objectFit='cover' />
+          <ModalHeader>{ig_info.title}</ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Text color={'gray.500'}>{props.ig_info.description}</Text>
+            <Text color={'gray.500'}>{ig_info.description}</Text>
           </ModalBody>
           <ModalFooter justifyContent='flex-start'>
-            <Box mr={2}>
-              <Button
-                overflow='hidden'
-                textOverflow={'ellipsis'}
-                leftIcon={<FaUserCircle />}
-                variant='outline'
-                colorScheme='blue'
-                minWidth={'8vw'}
-                maxWidth={'8vw'}
-                rounded='15px'
-              >
-                <Text noOfLines={[1]} display='inline'>
-                  {props.ig_info.contact}
-                </Text>
-              </Button>
-            </Box>
-            <Box>
-              <Link href={props.ig_info.invite_link} rel='noopener noreferrer' target='_blank'>
-                <Button
-                  overflow='hidden'
-                  textOverflow={'ellipsis'}
-                  variant='outline'
-                  colorScheme='blue'
-                  minWidth={'8vw'}
-                  maxWidth={'8vw'}
-                  rounded='15px'
-                >
-                  Invite Link
-                </Button>
-              </Link>
-            </Box>
+            <Box mr={2}>{getContactButton(ig_info.contact)}</Box>
+            {getInviteLinkButton(ig_info.invite_link)}
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -169,72 +156,3 @@ const IGCard: React.FC<IGInfoProps> = (props) => {
 }
 
 export default IGCard
-
-
-/*
-const LeftPane: React.FC<IGInfoProps> = ({ ig_info }) => {
-  const { contact, invite_link, image } = ig_info
-
-  return (
-    <VStack padding='1rem' borderRight='2px solid darkgrey'>
-      <Image objectFit='cover' maxW={{ base: '100%' }} src={image} alt='IG Picture' />
-      <Button
-        overflow='hidden'
-        textOverflow='ellipsis'
-        variant='outline'
-        colorScheme='blue'
-        leftIcon={<FaUserCircle />}
-        minWidth='8vw'
-        maxWidth='8vw'
-        style={{
-          borderRadius: '0.5rem',
-          color: '#1f407b',
-          border: '1px solid #1f407b',
-          display: 'flex',
-          alignItems: 'center',
-        }}
-      >
-        <Text noOfLines={[1]}>{contact}</Text>
-      </Button>
-      <Button
-        as={Link}
-        href={invite_link}
-        rel='noopener noreferrer'
-        target='_blank'
-        variant='outline'
-        minWidth='8vw'
-        maxWidth='8vw'
-        style={{ borderRadius: '0.5rem', color: '#229ed9', border: '1px solid #229ed9' }}
-      >
-        Invite Link
-      </Button>
-    </VStack>
-  )
-}
-
-const IGCard: React.FC<IGInfoProps> = ({ ig_info }) => {
-  const { title, description } = ig_info
-  return (
-    <Card
-      padding='1rem'
-      direction={{ base: 'column', sm: 'row' }}
-      overflow='hidden'
-      variant='outline'
-    >
-      <LeftPane ig_info={ig_info} />
-      <Stack>
-        <CardBody>
-          <Heading fontSize={'2xl'} fontFamily={'body'}>
-            {title}
-          </Heading>
-
-          <ExpandableText noOfLines={3}>
-            <Text color={'gray.500'}>{description}</Text>
-          </ExpandableText>
-        </CardBody>
-      </Stack>
-    </Card>
-  )
-}
-
- */
--- pages/student-groups.tsx
+++ pages/student-groups.tsx
@@ -1,27 +1,11 @@
 import type { NextPage } from 'next'
 import NavMenu from '../components/NavMenu'
-import { Flex, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react'
-import {
-  Box,
-  Card,
-  CardBody,
-  Checkbox,
-  CheckboxGroup,
-  Input,
-  InputGroup,
-  InputLeftElement,
-  StackDivider,
-  Stack,
-  Spacer,
-  Grid,
-  GridItem,
-} from '@chakra-ui/react'
+import { Box, Flex, Heading, HStack, SimpleGrid, VStack } from '@chakra-ui/react'
 import Footer from '../components/Footer'
 import IGCard from '../components/IGCard'
 import IGMockDetails from '../constants/IGMockData'
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import IGSearchFilter from '../components/IGSearchFilter'
-import { SearchIcon } from '@chakra-ui/icons'
 
 const StudentGroups: NextPage = () => {
   /* make requests to backend here */
@@ -34,12 +18,12 @@ const StudentGroups: NextPage = () => {
   const [interestGroupFilters, setInterestGroupFilters] = useState(originalFilters)
   const [interestGroupSearchString, setInterestGroupSearchString] = useState('')
 
-  const onInput = (ev: React.ChangeEvent<HTMLInputElement>) => {
+  const onInput = (ev: ChangeEvent<HTMLInputElement>) => {
     const { value } = ev.target
     setInterestGroupSearchString(value.toLowerCase())
   }
 
-  const onChange = (ev: React.ChangeEvent<HTMLInputElement>) => {
+  const onChange = (ev: ChangeEvent<HTMLInputElement>) => {
     const { value, checked } = ev.target
     if (checked) {
       setInterestGroupFilters([...interestGroupFilters, value])
@@ -66,71 +50,21 @@ const StudentGroups: NextPage = () => {
   }
 
   return (
-    // TODO M removed Flex here
-    <Flex justify='center' flexDir='column' as='main'>
-      <NavMenu />
-      <Grid templateColumns='repeat(4, 1fr)' gap={8}>
-        <GridItem colSpan={1}>
-          <Box p={'2vh'} pl={'7vh'}>
-            <Card
-              direction={{ base: 'column', sm: 'row' }}
-              overflow='hidden'
-              variant='outline'
-              shadow='lg'
-            >
-              <CardBody>
-                <InputGroup>
-                  <InputLeftElement pointerEvents='none'>
-                    <SearchIcon color='gray.300' />
-                  </InputLeftElement>
-                  <Input type='text' placeholder='Search for groups' />
-                </InputGroup>
-                <Box p={'2vh'}>
-                  <CheckboxGroup colorScheme='green' defaultValue={['arts', 'sports', 'gui']}>
-                    <Stack spacing={[1, 5]} direction={['column', 'column']}>
-                      <Checkbox value='arts'>Socio-cultural</Checkbox>
-                      <Checkbox value='sports'>Sports</Checkbox>
-                      <Checkbox value='gui'>GUIPs</Checkbox>
-                      <Checkbox value='inactive'>Inactive</Checkbox>
-                    </Stack>
-                  </CheckboxGroup>
-                </Box>
-              </CardBody>
-            </Card>
-          </Box>
-        </GridItem>
-
-        <GridItem colSpan={3}>
-          <Box pt={'2vh'}>
-            <Heading color={'gray.600'} fontSize={'2xl'} fontFamily={'header'}>
-              {IGDetails.length} Interest Groups
-            </Heading>
-          </Box>
-
-          <Box pt={'2vh'} pb={'3vh'}>
-            <SimpleGrid columns={[1, null, 2]} maxWidth={'95%'} spacing='40px'>
-              {IGDetails.map((IGDetail) => (
-                <IGCard key={IGDetail.title} ig_info={IGDetail} />
-              ))}
-            </SimpleGrid>
-          </Box>
-        </GridItem>
-      </Grid>
-      <Footer />
-    </Flex>
-  )
-}
-
-export default StudentGroups
-
-/*
-  return (
     <Flex justify='center' flexDir='column' as='main'>
       <NavMenu />
       <HStack pt='3rem' pb='3rem'>
-        <IGSearchFilter {...igSearchFilterProps} />
+        <Box p={'2vh'} pl={'7vh'}>
+          <IGSearchFilter {...igSearchFilterProps} />
+        </Box>
         <VStack flexGrow={1} pr='4rem' minH='45vh'>
-          <Heading as='h3' size='lg' fontWeight='normal' alignSelf='flex-start' mb='1.5rem'>
+          {/*<Box pt={"2vh"}>*/}
+          <Heading
+            fontFamily={'header'}
+            size='lg'
+            fontWeight='normal'
+            alignSelf='flex-start'
+            mb='1.5rem'
+          >
             {interestGroupCards.length > 0 ? (
               <>
                 <strong>{interestGroupCards.length}</strong> Interest Groups{' '}
@@ -139,14 +73,20 @@ export default StudentGroups
               <>No Interest Groups Found</>
             )}
           </Heading>
-          <SimpleGrid columns={[1, null, 2]} spacing='2rem' overflowY='auto'>
+          {/*</Box>*/}
+
+          {/*<Box pt={"2vh"} pb={"3vh"}>*/}
+          <SimpleGrid columns={[1, null, 2]} maxWidth={'95%'} spacing='2rem' overflowY='auto'>
             {interestGroupCards.map((interestGroupDetail, idx) => (
               <IGCard key={idx} ig_info={interestGroupDetail} />
             ))}
           </SimpleGrid>
         </VStack>
+        {/*</Box>*/}
       </HStack>
       <Footer />
     </Flex>
   )
- */
+}
+
+export default StudentGroups
