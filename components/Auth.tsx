96356d43946b7e3da72ef5bbfd4861915b20e14b
parth-io <67057645+parth-io@users.noreply.github.com>
2023-06-10 19:42:00 +0700

    Update Telegram JWT dev auth

--- components/Auth.tsx
+++ components/Auth.tsx
@@ -4,10 +4,10 @@ import { Button } from '@chakra-ui/react'
 import TelegramLoginButton from './TelegramLoginButton'
 import { useUserInfo } from '../utils'
 
-// { Parth: 4, Zhi Sheng: 5, Megan: 6, Conrad: 7 } based on 2 April seed file
-const NEXT_PUBLIC_BACKEND_TELEGRAM_USER_ID = 5
+// { Parth: 22, Zhi Sheng: 23, Megan: 24, Conrad: 25 } based on 2 April seed file
+const NEXT_PUBLIC_BACKEND_TELEGRAM_USER_ID = 22
 const NEXT_PUBLIC_BACKEND_JWT_DEV =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTExMzY0ODY1MCwiZmlyc3RfbmFtZSI6IlBhcnRoIiwidXNlcm5hbWUiOiJncGFydGgyNiIsImF1dGhfZGF0ZSI6MTY4MDQ5MDc0NywicGhvdG9fdXJsIjoiaHR0cHM6Ly90Lm1lL2kvdXNlcnBpYy8zMjAvZ0VtVHBfbDR1WUJueWQ3elZFZTQxVGRuQWhQczgtMmJnbXY3MXc4ZzM2US5qcGciLCJoYXNoIjoiNzVkNTc3ZmNhMjk0NGM2ZmE3ZTU3OTgwOGRhZDM1Yjg4M2U3ZTRkNTkzMTlhNjM3ZGUxNjA1N2VlNzhjYzAyNSIsImlhdCI6MTY4MDQ5MDc0OSwiZXhwIjoxNjgwNDk0MzQ5fQ.spKGHWq8Mmg-jARg2J3mgsN6wupsHl6xxMfom3DeqSs'
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTExMzY0ODY1MCwiZmlyc3RfbmFtZSI6IlBhcnRoIiwidXNlcm5hbWUiOiJncGFydGgyNiIsImF1dGhfZGF0ZSI6MTY4NjQwMDEzNCwicGhvdG9fdXJsIjoiaHR0cHM6Ly90Lm1lL2kvdXNlcnBpYy8zMjAvZ0VtVHBfbDR1WUJueWQ3elZFZTQxVGRuQWhQczgtMmJnbXY3MXc4ZzM2US5qcGciLCJoYXNoIjoiNGNkOWE1YzQ1NTJiMjQ5MDc3ZGVlYTQ4MDhmYmZhNDVmMjg4Zjg1MDNlYTE1YTliMzFiNmQzM2M1ODE3ZWQ2MCIsImlhdCI6MTY4NjQwMDEzNywiZXhwIjoxNjg2NDAzNzM3fQ.xG7DIGg3ZFZ5flEzl_nqz-LRDPaUfpWl9jP9kjhffE8'
 const NEXT_PUBLIC_NGINX_PROXY_ON = false
 
 const Auth: React.FC = () => {
@@ -18,7 +18,7 @@ const Auth: React.FC = () => {
       onClick={() => {
         setAuth({
           token: NEXT_PUBLIC_BACKEND_JWT_DEV,
-          orgIds: [0],
+          orgIds: [66, 67],
           userId: NEXT_PUBLIC_BACKEND_TELEGRAM_USER_ID,
           // userInfo is not needed for now, so can just add filler values
           userInfo: {
