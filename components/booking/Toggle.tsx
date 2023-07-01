7a1f7de1217c3c09fc663228dba7f97bfc66f1d1
sheng <chengzhisheng2001@gmail.com>
2023-07-01 15:31:19 +0800

    Minor UI fixes
    Change toggle to "Day" and "Mth" instead of icons. Made the menu bar
    for venues wider to avoid changes in size.

--- components/booking/Toggle.tsx
+++ components/booking/Toggle.tsx
@@ -1,6 +1,7 @@
 import { useRef } from 'react'
 import { motion, AnimatePresence } from 'framer-motion'
 import { FaSun, FaCalendar } from 'react-icons/fa'
+import { HStack, Text } from '@chakra-ui/react'
 
 const Toggle: React.FC<ToggleProps> = ({ isOn, setIsOn }) => {
   const STYLES = {
@@ -32,24 +33,20 @@ const Toggle: React.FC<ToggleProps> = ({ isOn, setIsOn }) => {
   }
 
   const ref = useRef<HTMLDivElement>(null)
+  const scrollToTop = () => {
+    setTimeout(
+      () =>
+        window.scrollTo({
+          top: 0.1 * document.documentElement.clientHeight,
+          behavior: 'smooth',
+        }),
+      0,
+    )
+    setIsOn(!isOn)
+  }
 
   return (
-    <div
-      className='container'
-      onClick={() => {
-        setTimeout(
-          () =>
-            window.scrollTo({
-              top: 0.1 * document.documentElement.clientHeight,
-              behavior: 'smooth',
-            }),
-          0,
-        )
-        setIsOn(!isOn)
-      }}
-      style={STYLES.container}
-      ref={ref}
-    >
+    <div className='container' onClick={scrollToTop} style={STYLES.container} ref={ref}>
       <motion.div layout className='handle' style={STYLES.handle}>
         <AnimatePresence mode='wait' initial={false}>
           <motion.span
@@ -59,7 +56,7 @@ const Toggle: React.FC<ToggleProps> = ({ isOn, setIsOn }) => {
             animate={{ y: 0, opacity: 1 }}
             transition={{ duration: 0.2 }}
           >
-            {isOn ? <FaCalendar /> : <FaSun />}
+            {isOn ? <Text fontSize="xs">Mth</Text> : <Text fontSize="xs">Day</Text>}
           </motion.span>
         </AnimatePresence>
       </motion.div>
