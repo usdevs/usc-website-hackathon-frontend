469a876cdd36dcfd7b7fdb8c63b007e3357537ee
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-12-28 18:48:55 +0700

    Add default folio page

---
+++ pages/folio.tsx
@@ -0,0 +1,14 @@
+import Footer from "../components/Footer";
+
+const folio = () => {
+  return (
+    <>
+      <div>
+        <h1>Folio Page</h1>
+      </div>
+      <Footer/>
+    </>
+  );
+};
+
+export default folio;
