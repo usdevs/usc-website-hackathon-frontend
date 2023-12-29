8c794d9bfb84e76c89c0066702290e4051431e8b
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-12-29 18:38:51 +0700

    Add IconButtons

--- components/folio/EssayCard.tsx
+++ components/folio/EssayCard.tsx
@@ -1,16 +1,12 @@
-import { Card, CardHeader, CardBody, CardFooter, Text, Heading, Image, HStack, VStack } from '@chakra-ui/react'
+import { DeleteIcon, EditIcon, ViewIcon } from '@chakra-ui/icons';
+import { Card, CardHeader, CardBody, CardFooter, Text, Heading, Avatar, HStack, VStack, IconButton } from '@chakra-ui/react'
 
 const EssayCard = () => {
     return (
         <Card>
             <CardHeader>
                 <HStack spacing='10px'>
-                    <Image
-                        borderRadius='full'
-                        boxSize='50px'
-                        src='https://bit.ly/dan-abramov'
-                        alt='Profile Picture'
-                    />
+                    <Avatar src='https://bit.ly/broken-link' />
                     <VStack>
                         <Heading size='md'>Essay Title</Heading>
                         <Text>Student Name</Text> 
@@ -20,6 +16,23 @@ const EssayCard = () => {
             <CardBody>
                 <p>This sentence is the first sentence of the essay that is to be displayed, or it could be snippets of the essay.</p>
             </CardBody>
+            <CardFooter gap='5px'>
+                <IconButton
+                    variant='outline'
+                    aria-label='View Essay'
+                    icon={<ViewIcon />} 
+                />
+                <IconButton
+                    variant='outline'
+                    aria-label='Edit Essay'
+                    icon={<EditIcon />} 
+                />
+                <IconButton
+                    variant='outline'
+                    aria-label='Delete Essay'
+                    icon={<DeleteIcon />} 
+                />
+            </CardFooter>
         </Card>
     );
 }
