ad3df4c105ae7096f1abfa1095a1708d78858739
Timothy Putra Prasetio <timothypp02@gmail.com>
2023-12-29 18:21:34 +0700

    Refactor card layout using stack

--- components/folio/EssayCard.tsx
+++ components/folio/EssayCard.tsx
@@ -1,22 +1,25 @@
-import { Card, CardHeader, CardBody, CardFooter, Text, Heading, Image } from '@chakra-ui/react'
+import { Card, CardHeader, CardBody, CardFooter, Text, Heading, Image, HStack, VStack } from '@chakra-ui/react'
 
 const EssayCard = () => {
     return (
         <Card>
             <CardHeader>
-                <Image
-                    borderRadius='full'
-                    boxSize='150px'
-                    src='https://bit.ly/dan-abramov'
-                    alt='Dan Abramov'
-                />
-                <Heading size='md'>Essay Title</Heading>
-                <Text>Student Name</Text>
+                <HStack spacing='10px'>
+                    <Image
+                        borderRadius='full'
+                        boxSize='50px'
+                        src='https://bit.ly/dan-abramov'
+                        alt='Profile Picture'
+                    />
+                    <VStack>
+                        <Heading size='md'>Essay Title</Heading>
+                        <Text>Student Name</Text> 
+                    </VStack>
+                </HStack>
             </CardHeader>
             <CardBody>
-                <p>Lorem ipsum</p>
+                <p>This sentence is the first sentence of the essay that is to be displayed, or it could be snippets of the essay.</p>
             </CardBody>
-            <CardFooter>Date</CardFooter>
         </Card>
     );
 }
