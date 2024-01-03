195332b1fcbb1bb91af53a4f16cf979422d6388d
iuhiah <leehaihui2004@gmail.com>
2024-01-03 16:32:23 +0800

    make navbar responsive

--- components/NavMenu.tsx
+++ components/NavMenu.tsx
@@ -1,14 +1,8 @@
 import React from 'react'
 import {
-  Box,
-  Container,
-  Flex,
-  HStack,
+  Box, Flex, HStack, Stack, Text,
   IconButton,
-  LinkBox,
-  LinkOverlay,
-  Stack,
-  Text,
+  LinkBox, LinkOverlay,
   useDisclosure,
 } from '@chakra-ui/react'
 import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons'
@@ -18,12 +12,9 @@ import { CldImage } from 'next-cloudinary'
 
 const NavLink: React.FC<NavigationLink> = (props) => (
   <LinkBox
-    px={2}
-    py={1}
+    px={2} py={1}
     boxShadow={'inset 0 0 0 0 white'}
     color='white'
-    margin='0 -.25rem'
-    padding='0.25rem'
     transition='color .3s ease-in-out, box-shadow .3s ease-in-out'
     _hover={{
       color: '#ff9900',
@@ -53,47 +44,49 @@ const NavMenu = () => {
 
   return (
     <>
-      <LinkBox>
-        <Box display={'flex'} justifyContent={'center'} padding='0.5rem'>
-          {/*<Heading as='h1' size='2xl' color='#1f407b' fontFamily={'Blender-Medium'}>*/}
-          {/*  NUS{' '}*/}
-          {/*  <Text as='span' color='#ef7c00'>*/}
-          {/*    College*/}
-          {/*  </Text>{' '}*/}
-          {/*  Club*/}
-          {/*</Heading>*/}
-          <CldImage src={'nusc-logo'} alt={'NUS College'} height={420} width={190} />
-          <LinkOverlay href={'/'} />
-        </Box>
-      </LinkBox>
-      <Box px={4} bg='#1f407b'>
-        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
-          <IconButton
+    <Box bg='#1f407b'padding='1.5vh'>
+      <Flex justifyContent={'center'} alignItems={'center'} position={'relative'} h={'10vh'}>
+        <IconButton
             size={'md'}
             icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
             aria-label={'Open Menu'}
             display={{ md: 'none' }}
+            position={'absolute'}
+            top={'50%'} left={'0'} transform={'translateY(-50%)'}
             onClick={isOpen ? onClose : onOpen}
           />
-          <Container centerContent>
-            {/* Links to various pages in the Nav bar (IGs, NOW, etc.) */}
-            <HStack as={'nav'} spacing={36} display={{ base: 'none', md: 'flex' }}>
-              <NavLinks />
-            </HStack>
-          </Container>
-          <Flex alignItems={'center'}>
-            <Auth />
-          </Flex>
-        </Flex>
+        <LinkBox display={'flex'} justifyContent={'center'} borderRadius={10} overflow={'hidden'}>
+            <CldImage src={'nusc-logo'} alt={'NUS College'} height={420} width={190} />
+            <LinkOverlay href={'/'} />
+        </LinkBox>
+        {/* TODO: CHANGE ICON */}
+        <Box position={'absolute'} top={'50%'} right={'0'} transform={'translateY(-50%)'}>
+          <Auth />
+        </Box>
+      </Flex>
+      {/* Links to various pages in the Nav bar (IGs, NOW, etc.) */}
+      <HStack as={'nav'} w={'80%'} display={{ base: 'none', md: 'flex'}}
+        justifyContent={'space-between'} margin={'auto'} paddingTop='1rem'>
+        <NavLinks />
+      </HStack>
+    </Box>
 
-        {isOpen ? (
-          <Box pb={4} display={{ md: 'none' }}>
-            <Stack as={'nav'} spacing={4}>
-              <NavLinks />
-            </Stack>
-          </Box>
-        ) : null}
+    {isOpen ? (
+      <Box bg='#1f407b' w={'fit-content'} h={'100vh'} p={'1.5vh'}
+        position={'fixed'} top={'0'} zIndex={'100'}>
+        <Flex h={'10vh'} alignItems={'center'}>
+          <IconButton
+              size={'md'}
+              icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+              aria-label={'Open Menu'}
+              onClick={isOpen ? onClose : onOpen}
+            />
+        </Flex>
+        <Stack as={'nav'} spacing={4}>
+          <NavLinks />
+        </Stack>
       </Box>
+    ) : null}
     </>
   )
 }
